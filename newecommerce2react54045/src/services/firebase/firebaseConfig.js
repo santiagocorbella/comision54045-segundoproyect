be0ca97476e3e9caa0dd46cc179ad4ab5b7ce315
santiagocorbella <santicorbella@gmail.com>
2024-09-25 19:12:19 -0300

    vercel-variables-de-entorno

--- newecommerce2react54045/src/services/firebase/firebaseConfig.js
+++ newecommerce2react54045/src/services/firebase/firebaseConfig.js
@@ -2,15 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVmQbTuriE-bmo5bGL4OsSx6K0cWlGXSA",
-  authDomain: "backend54045segundoproyecto.firebaseapp.com",
-  projectId: "backend54045segundoproyecto",
-  storageBucket: "backend54045segundoproyecto.appspot.com",
-  messagingSenderId: "677891632002",
-  appId: "1:677891632002:web:6add3394b83bcce5b77d9b"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
+
+ /*apiKey: "AIzaSyCVmQbTuriE-bmo5bGL4OsSx6K0cWlGXSA",
+  authDomain: "backend54045segundoproyecto.firebaseapp.com",
+  projectId: "backend54045segundoproyecto",
+  storageBucket: "backend54045segundoproyecto.appspot.com",
+  messagingSenderId: "677891632002",
+  appId: "1:677891632002:web:6add3394b83bcce5b77d9b"
+  */
