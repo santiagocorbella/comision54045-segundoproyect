0b7ee7f53d2f72ec417ba3764d2896c96a1fc2a4
santiagocorbella <santicorbella@gmail.com>
2024-10-01 19:19:17 -0300

    tercera-entrega-final-footer

--- newecommerce2react54045/src/App.jsx
+++ newecommerce2react54045/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import  Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -7,30 +7,34 @@ import { CartProvider } from './context/CartContext'
 import { NotificationProvider } from './notification/NotificationsService'
 import CartView from './components/CartView/CartView'
 import Checkout from './components/Checkout/Checkout'
+import Footer from './components/Footer/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-
- return (
-   <>
+  return (
     <BrowserRouter>
-     <NotificationProvider>
-      <CartProvider>
-       <Navbar/>
-       <Routes>
-        <Route path='/' element ={<ItemListContainer greeting={'¡Bienvenidos!'}/>}/>
-        <Route path='/category/:categoryId' element={<ItemListContainer greeting={'¡Productos de la categoria!'}/>}/>
-        <Route path= '/item/:itemId' element={<ItemDetailContainer />} />
-        <Route path='/cart' element={<CartView/>}/>
-        <Route path='/checkout' element={<Checkout/>}/>
-       </Routes>
-      </CartProvider>
-     </NotificationProvider>
-    </BrowserRouter> 
-   </>
-  )
+      <NotificationProvider>
+        <CartProvider>
+          <div className="d-flex flex-column min-vh-100"> 
+            <Navbar />
+            <main className="flex-fill"> 
+              <Routes>
+                <Route path='/' element={<ItemListContainer greeting={'¡Bienvenidos!'} />} />
+                <Route path='/category/:categoryId' element={<ItemListContainer greeting={'¡Productos de la categoria!'} />} />
+                <Route path='/item/:itemId' element={<ItemDetailContainer />} />
+                <Route path='/cart' element={<CartView />} />
+                <Route path='/checkout' element={<Checkout />} />
+              </Routes>
+            </main>
+            <Footer /> 
+          </div>
+        </CartProvider>
+      </NotificationProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App  
+export default App
 
 
 
---
+++ newecommerce2react54045/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer style={{
+                background: 'linear-gradient(to right, hsl(60, 93%, 50%), #f68e08, #f84b06, #f70606, #de06c5)',
+                textDecoration: 'underline', margin: 0,
+                color: 'darkblue',
+                textAlign: 'center',
+                padding: '20px',
+              
+            }}>
+            <h1>Sitio web hecho por Santiago Eloy Corbella</h1>
+        </footer>
+    );
+}
+
+export default Footer
+
+
